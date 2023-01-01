831b79cf4960450defce677720930897f333e1d6
PUJAPURVI <nerkarpuja@gmail.com>
2023-01-01 15:12:26 +0530

    Added: Welcome Message

---
+++ EmployeeWage.java
@@ -0,0 +1,7 @@
+package com.employeWage;
+
+public class EmployeeWage {
+    public static void main(String[] args) {
+        System.out.println(".....WEL-COME TO THE EMPLOYEE WAGE COMPUTATION PROGRAM.....");
+    }
+}
