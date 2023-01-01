0a9f3efb1e16495521614f298cd687b65bc13238
PUJAPURVI <nerkarpuja@gmail.com>
2023-01-01 15:28:01 +0530

    Added: Employee Attendance

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,7 +1,15 @@
 package com.employeWage;
-
+import java.util.Random;
+import java.util.Scanner;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println(".....WEL-COME TO THE EMPLOYEE WAGE COMPUTATION PROGRAM.....");
+        Random r = new Random();
+        int empcheck = r.nextInt(2) + 1;
+        if (empcheck == 1) {
+            System.out.println("Employee is present");
+        } else {
+            System.out.println("Employee Is Absent");
+        }
     }
 }
